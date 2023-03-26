98a9365eb5d6f9b34d4086730f7d14c27ea0238a
TeilorVladimir <giever2222@gmail.com>
2023-03-26 12:26:39 +0300

    Initial Commit

---
+++ sliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sliders
+//
+//  Created by Сораб В Тейлор on 26.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
